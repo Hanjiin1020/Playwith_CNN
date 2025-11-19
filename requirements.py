2e9b10c5addcf3116e35a32b7f03331eceb540e6
Hanjiin1020 <81553092+Hanjiin1020@users.noreply.github.com>
2025-11-19 10:00:45 +0900

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,9 @@
+import torch
+import torchvision
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import PIL
+import torch.utils.data as DataLoader
+import torch.optim as optim
