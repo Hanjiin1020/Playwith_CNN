1dad8574eaa4150b55e3d4ac05effcd09fe876ee
Hanjiin1020 <81553092+Hanjiin1020@users.noreply.github.com>
2025-11-19 10:08:48 +0900

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,1 +1,14 @@
-
+cifar_resize = (32, 32)
+mean = (0.5, 0.5, 0.5)
+std = (0.5, 0.5, 0.5)
+transform_train = transforms.Compose([
+    transforms.Resize(cifar_resize, interpolation=PIL.Image.BICUBIC),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
+transform_test = transforms.Compose([
+    transforms.Resize(cifar_resize, interpolation=PIL.Image.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize(mean, std)
+])
